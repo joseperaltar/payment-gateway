adcbd7238555680e85ef6dccd23b856e7349367e
Jose Peralta <jperalta@bancomercantil.com>
2024-05-26 15:14:31 -0400

    t

--- src/app/testing/testing.component.ts
+++ src/app/testing/testing.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FetchJsonService } from '../services/fetch-json.service';
 import b2b from '../models/b2b';
-import * as B2B from "../../assets/domains/b2b/b2b-pay.json";
+import * as B2B from "../domains/b2b/b2b-pay.json";
 
 @Component({
   selector: 'app-testing',
@@ -13,11 +13,6 @@ import * as B2B from "../../assets/domains/b2b/b2b-pay.json";
 export class TestingComponent {
   b2b: b2b = new b2b("");
   constructor(private json: FetchJsonService) {
-  }
-
-ngOnInit() {
-    this.json.loadInfo().subscribe( (info:any) => {
-      this.b2b.title = B2B.title
-    })
+    this.b2b.title = B2B.title
   }
 }
